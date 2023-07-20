9ab1095f09de4f5731a4c62df14dc465b824921a
Giacomo Giudice <ggiac93@gmail.com>
2023-07-20 18:52:23 +0200

    Update benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,24 +5,44 @@ using Random
 # Set seed for reproducibility
 Random.seed!(42)
 
-# Check if using BMI2
-@info "USE_BMI2 = $(BitPermutations.USE_BMI2)"
+# Check if using instrinsics
+@assert BitPermutations.USE_BMI2 && BitPermutations.USE_AVX512
 
 # Struct in which to save benchmark results
 suite = BenchmarkGroup()
 test_types = (UInt8, UInt16, UInt32, UInt64, UInt128)
+avx_types = (UInt16, UInt32, UInt64)
 N = 10_000
 
 # Generate results for benchmarking bit permutations on a single bitstring of type `T`
 suite["Single"] = BenchmarkGroup()
 
-rand_perm(::Type{T}) where {T} = shuffle!(collect(1:bitsize(T)))
-benes_perm(::Type{T}) where {T} = BitPermutation{T}(rand_perm(T); type=BenesNetwork)
-grp_perm(::Type{T}) where {T} = BitPermutation{T}(rand_perm(T); type=GRPNetwork)
+rand_perm(::Type{T}) where {T} = randperm(bitsize(T))
+benes_perm(::Type{T}) where {T} = BitPermutation{T}(randperm(bitsize(T)); type=BenesNetwork)
+grp_perm(::Type{T}) where {T} = BitPermutation{T}(randperm(bitsize(T)); type=GRPNetwork)
+avx_perm(::Type{T}) where {T} = BitPermutation{T}(randperm(bitsize(T)); type=AVXCopyGather)
 
 for T in test_types
     group = suite["Single"][T] = BenchmarkGroup()
 
+    group["BitVector"] = @benchmarkable begin
+        @inbounds for _ in 1:($N)
+            permute!(v, p)
+        end
+        return v
+    end setup = begin
+        v = BitVector(Bits(rand($T)))
+        p = rand_perm($T)
+    end
+    group["Bits"] = @benchmarkable begin
+        @inbounds for _ in 1:($N)
+            permute!(b, p)
+        end
+        return b
+    end setup = begin
+        b = Bits(rand($T))
+        p = rand_perm($T)
+    end
     group["BenesNetwork"] = @benchmarkable begin
         @inbounds for _ in 1:($N)
             x = bitpermute(x, p)
@@ -41,23 +61,15 @@ for T in test_types
         x = rand($T)
         p = grp_perm($T)
     end
-    group["Bits"] = @benchmarkable begin
+    T in avx_types || continue
+    group["AVXCopyGather"] = @benchmarkable begin
         @inbounds for _ in 1:($N)
-            permute!(b, p)
+            x = bitpermute(x, p)
         end
-        return b
+        return x
     end setup = begin
-        b = Bits(rand($T))
-        p = rand_perm($T)
-    end
-    group["BitVector"] = @benchmarkable begin
-        @inbounds for _ in 1:($N)
-            permute!(v, p)
-        end
-        return v
-    end setup = begin
-        v = BitVector(Bits(rand($T)))
-        p = rand_perm($T)
+        x = rand($T)
+        p = avx_perm($T)
     end
 end
 
@@ -66,13 +78,12 @@ suite["Broadcasted"] = BenchmarkGroup()
 for T in test_types
     group = suite["Broadcasted"][T] = BenchmarkGroup()
 
-    group["BenesNetwork"] = @benchmarkable bitpermute!(p, xs) setup = begin
-        xs = rand($T, $N)
-        p = benes_perm($T)
-    end
-    group["GRPNetwork"] = @benchmarkable bitpermute!(p, xs) setup = begin
-        xs = rand($T, $N)
-        p = grp_perm($T)
+    group["BitVector"] = @benchmarkable begin
+        foreach(v -> permute!(v, p), vs)
+        return vs
+    end setup = begin
+        vs = [BitVector(Bits(rand($T))) for _ in 1:($N)]
+        p = rand_perm($T)
     end
     group["Bits"] = @benchmarkable begin
         foreach(b -> permute!(b, p), bs)
@@ -81,12 +92,18 @@ for T in test_types
         bs = [Bits(rand($T)) for _ in 1:($N)]
         p = rand_perm($T)
     end
-    group["BitVector"] = @benchmarkable begin
-        foreach(v -> permute!(v, p), vs)
-        return vs
-    end setup = begin
-        vs = [BitVector(Bits(rand($T))) for _ in 1:($N)]
-        p = rand_perm($T)
+    group["BenesNetwork"] = @benchmarkable bitpermute!(xs, p) setup = begin
+        xs = rand($T, $N)
+        p = benes_perm($T)
+    end
+    group["GRPNetwork"] = @benchmarkable bitpermute!(xs, p) setup = begin
+        xs = rand($T, $N)
+        p = grp_perm($T)
+    end
+    T in avx_types || continue
+    group["AVXCopyGather"] = @benchmarkable bitpermute!(xs, p) setup = begin
+        xs = rand($T, $N)
+        p = avx_perm($T)
     end
 end
 
--- benchmark/plot.jl
+++ benchmark/plot.jl
@@ -2,8 +2,9 @@ using BenchmarkTools
 using Plots
 using Plots.Measures
 
+bench_group = ["BenesNetwork", "GRPNetwork", "AVXCopyGather", "Bits", "BitVector"]
 base_types = (UInt8, UInt16, UInt32, UInt64, UInt128)
-bench_group = ["BenesNetwork", "GRPNetwork", "Bits", "BitVector"]
+avx_types = (UInt16, UInt32, UInt64)
 N = 10_000
 
 fname = nothing
@@ -15,14 +16,16 @@ end
 gap = 2
 tick_locations = eachindex(base_types) * (length(bench_group) + gap) .+ length(bench_group) / 2
 
+get_time(group, key) = haskey(group, key) ? time(group[key]) : NaN
+
 p₁ = plot(; title="Single repeated permutation", legend=:bottomright)
 xs = [[eachindex(base_types) * (length(bench_group) + gap) .+ k] for k in eachindex(bench_group)]
-ys = [[time(results["Single"][T][g]) / N for T in base_types] for g in bench_group]
+ys = [[get_time(results["Single"][T], g) / N for T in base_types] for g in bench_group]
 scatter!(xs, ys; seriescolor=permutedims(1:length(bench_group)), markersize=6, labels=permutedims(bench_group))
 
 p₂ = plot(; labels=permutedims(bench_group), title="In-place broadcasted permutation", legend=:none)
 xs = [[eachindex(base_types) * (length(bench_group) + gap) .+ k] for k in eachindex(bench_group)]
-ys = [[time(results["Broadcasted"][T][g]) / N for T in base_types] for g in bench_group]
+ys = [[get_time(results["Broadcasted"][T], g) / N for T in base_types] for g in bench_group]
 scatter!(xs, ys; seriescolor=permutedims(1:length(bench_group)), markersize=6, labels=permutedims(bench_group))
 
 p = plot(
